5bb807a5de4399065705d591b6491cb87d622b40
AlejandroAngelopoulos <alexander.angelopoulos.it@gmail.com>
2022-06-02 09:10:19 +0300

    2/6/2022 09:10

---
+++ MyContactManagerData/MyContactManagerDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MyContactManagerData
+{
+    public class MyContactManagerDBContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+        
+        public MyContactManagerDBContext()
+        {
+            //purposefully empty but necessary for Scaffold
+
+        }
+        
+        public MyContactManagerDBContext(DbContextOptions options) : base(options)
+        {
+            //purposefully empty but necessary to set options properly
+        }
+
+        protected override OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json", optional:true ,reloadOnChange:true);
+            _configuration = builder.Build();
+            var constr = _configuration.GetConnectionString("MyContactManager");
+            optionsBuilder.UseSqlServer(constr);
+
+        }
+    }
+}
